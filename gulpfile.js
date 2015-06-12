dd6efcbe3609683646704e76f12b1aa65a886193
Alex Coady <alexcoady@stinkdigital.com>
2015-06-12 11:03:54 +0100

    No single quotes allowed

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 var gulp = require("gulp");
 var nodemon = require("gulp-nodemon");
 
-gulp.task('start', function () {
+gulp.task("start", function () {
   nodemon({
-    script: 'index.js'
-  , ext: 'js html'
-  , env: { 'NODE_ENV': 'development' }
+    script: "index.js"
+  , ext: "js html"
+  , env: { "NODE_ENV": "development" }
   })
 })
